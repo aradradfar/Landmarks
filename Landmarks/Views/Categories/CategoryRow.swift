41e1ac8ccdbb0e837a3396f09966f8a479c1f253
Arad Radfar <aradradfar@gmail.com>
2022-08-02 21:30:29 +0430

    created views for category rows and lists. Added tab views where featured landmarks are displayed.

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Arad Radfar on 7/31/22.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+
+    var body: some View {
+        VStack {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(items) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+    static var previews: some View {
+        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(4)))
+    }
+}
